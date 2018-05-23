6f98bea2609454ce8379f7b73032448d14602d9c
Arthur_OLLIER <285212@supinfo.com>
2018-05-23 17:16:31 +0200

    Facebook and Google Profiles

--- client/src/components/Profil/Profil.js
+++ client/src/components/Profil/Profil.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Profil.css';
+import './../Common/OAuth.css';
 import { connect } from 'react-redux';
 
 class ProfilPage extends Component {
@@ -9,17 +10,22 @@ class ProfilPage extends Component {
                 <div className="page-header" id="banner">
                     <div className="row">
                         <div className="col-lg-8 col-md-7 col-sm-6">
-                            <h1>Profil</h1>
+                            <div className="row">
+                            <h1 className="col-sm-3">Profil</h1>
                             {(this.props.user && this.props.user.google !== undefined && this.props.user.google !== null) &&
-                                <p>Connecté avec google : {this.props.user.google}</p>
+                                <div className="col-sm-9 loginBtn Add--google">Connecté avec google</div>
                             }
+                            {(this.props.user && this.props.user.facebook !== undefined && this.props.user.facebook !== null) &&
+                                <div className="col-sm-9 loginBtn Add--facebook">Connecté avec facebook</div>
+                            }
+                            </div>
                             <p className="lead">Gérer vos informations utilisateur</p>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-sm-3">
                             {(this.props.user && ((this.props.user.google !== undefined && this.props.user.google !== null)||this.props.user.facebook !== undefined && this.props.user.facebook !== null)) &&
-                                <img src={this.props.user.picture}/>
+                                <img className="profilePicture" src={this.props.user.picture}/>
                             }
                         </div>
                         <div className="row">
