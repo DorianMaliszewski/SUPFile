ade5e7128095b43d7ab82a7f2b6f91be6462b9aa
Esquiro69 <280407@supinfo.com>
2018-06-06 14:13:01 +0200

    Page d'intro

--- client/src/components/IntroPage.js
+++ client/src/components/IntroPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './IntroPage.css';
 
 class IntroPage extends Component {
     render() {
@@ -6,15 +7,41 @@ class IntroPage extends Component {
             <div className="container">
                 <div className="page-header" id="banner">
                     <div className="row">
-                    <div className="col-lg-8 col-md-7 col-sm-6">
-                        <h1>Voici SUPFile</h1>
-                        <p className="lead">Stocker et Partager vos fichiers</p>
-                    </div>
-                    <div className="col-lg-4 col-md-5 col-sm-6">
-                        <div className="sponsor">
-                        <script async="" type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&amp;serve=C6AILKT&amp;placement=bootswatchcom" id="_carbonads_js"></script>
+                        <div className="col-lg-8 col-md-7 col-sm-6">
+                            <h1>Voici SUPFile</h1>
+                            <p className="lead">Stocker et Partager vos fichiers</p>
                         </div>
-                    </div>
+                        <div className="col-lg-4 col-md-5 col-sm-6">
+                            <div className="sponsor">
+                            <script async="" type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&amp;serve=C6AILKT&amp;placement=bootswatchcom" id="_carbonads_js"></script>
+                            </div>
+                        </div>
+                        <div class="affichageIntro">
+                            <div class="partieAffichageIntro">
+                                <h2>Votre cloud de qualité</h2>
+                                <img class="tailleImg" src="/nuage.png"/>
+                                <p>
+                                    Choissisez un cloud de qualité avec SupFile.
+                                    Votre cloud vous permet de toujours avoir vos fichier avec vous.
+                                </p>
+                            </div>
+                            <div class="partieAffichageIntro reverse">
+                                <h2>Des serveurs puissants et performants</h2>
+                                <img class="tailleImg" src="https://upload.wikimedia.org/wikipedia/commons/6/67/Inside_Suite.jpg" />
+                                <p>
+                                    Choissisez un cloud de qualité avec SupFile.
+                                    Votre cloud vous permet de toujours avoir vos fichier avec vous.
+                                </p>
+                            </div>
+                            <div class="partieAffichageIntro">
+                                <h2>Votre cloud de qualité</h2>
+                                <img class="tailleImg" src="https://upload.wikimedia.org/wikipedia/commons/9/9e/Samsung_Galaxy_Note_5%2C_S6_edge%2B_and_Note_7_20161010b.jpg" />
+                                <p>
+                                    Choissisez un cloud de qualité avec SupFile.
+                                    Votre cloud vous permet de toujours avoir vos fichier avec vous.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
